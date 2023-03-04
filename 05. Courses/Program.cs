6e5ca67d29ac6ab86e72b619e2212d08b21c61b8
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-04 13:33:29 +0200

    Add files via upload

---
+++ 05. Courses/Program.cs
@@ -0,0 +1,36 @@
+﻿
+var courses = new Dictionary<string, List<string>>();
+
+while (true)
+{
+    string[] inputInfo = Console.ReadLine()
+    .Split(" : ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+    string courseName = inputInfo[0];
+
+    if (courseName == "end")
+    {
+        break;
+    }
+
+    string student = inputInfo[1];
+
+
+    if (!courses.ContainsKey(courseName))
+    {
+        courses.Add(courseName, new List<string>());
+    }
+
+    courses[courseName].Add(student);
+}
+
+foreach (var courseName in courses)
+{
+    Console.WriteLine($"{courseName.Key}: {courseName.Value.Count}");
+
+    foreach (var student in courseName.Value)
+    {
+        Console.WriteLine($"-- {student}");
+    }
+}
