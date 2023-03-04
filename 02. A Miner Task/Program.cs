325fd54899d26eaaefd215ebbb5e64a0b46dfaf7
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-04 12:23:06 +0200

    Add files via upload

---
+++ 02. A Miner Task/Program.cs
@@ -0,0 +1,25 @@
+﻿var resources = new Dictionary<string, int>();
+
+while (true)
+{
+    string item = Console.ReadLine();
+
+	if (item == "stop")
+	{
+		break;
+	}
+
+	int quantity = int.Parse(Console.ReadLine());
+
+	if (!resources.ContainsKey(item))
+	{
+		resources.Add(item, 0);
+	}
+
+    resources[item] += quantity;
+}
+
+foreach (var item in resources)
+{
+	Console.WriteLine($"{item.Key} -> {item.Value}");
+}
