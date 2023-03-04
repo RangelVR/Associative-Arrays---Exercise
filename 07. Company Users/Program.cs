ae698ea71606529d39981160ae199a710991c488
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-04 14:24:48 +0200

    Add files via upload

---
+++ 07. Company Users/Program.cs
@@ -0,0 +1,35 @@
+﻿var companies = new Dictionary<string, List<string>>();
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] cmdArg = input.Split(" -> ");
+
+    string nameCompany = cmdArg[0];
+    string iD = cmdArg[1];
+
+    if (!companies.ContainsKey(nameCompany))
+    {
+        companies.Add(nameCompany, new List<string> { iD });
+    }
+    else
+    {
+        if (!companies[nameCompany].Contains(iD))
+        {
+            companies[nameCompany].Add(iD);
+        }
+    }
+
+    input = Console.ReadLine();
+}
+
+foreach (var company in companies)
+{
+    Console.WriteLine(company.Key);
+
+    foreach (var iD in company.Value)
+    {
+        Console.WriteLine($"-- {iD}");
+    }
+}
