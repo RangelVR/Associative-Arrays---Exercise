276b971ee1345b83c35a23834e10568a78a1072a
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-04 12:21:56 +0200

    Add files via upload

---
+++ 01. Count Chars in a String/Program.cs
@@ -0,0 +1,24 @@
+﻿string text = Console.ReadLine();
+
+var characters = new Dictionary<char, int>();
+
+foreach (var ch in text)
+{
+	if (ch == ' ')
+	{
+		continue;
+	}
+
+	if (!characters.ContainsKey(ch))
+	{
+		characters.Add(ch, 0);
+	}
+
+	characters[ch]++;
+}
+
+foreach (var ch in characters)
+{
+	Console.WriteLine($"{ch.Key} -> {ch.Value}");
+}
+
